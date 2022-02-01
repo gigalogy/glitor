361037248da0ce600f73c526e80c63118ceb1a02
Md Moin Uddin <moensam@gmail.com>
2022-02-01 22:20:48 +0900

    add poetry to the running shell

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 echo "Installing Poetry.."
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python3 -
 
+source $HOME/.poetry/env
 echo "Poetry Version"
 poetry --version
 poetry config virtualenvs.create true
