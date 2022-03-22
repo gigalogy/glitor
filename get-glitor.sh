4a81bfa6237988ed44aebce8ee3ef07e375c7f20
Ghazanfar <skghazanfar@gmail.com>
2022-03-22 14:52:55 +0600

    get shell updated

--- get-glitor.sh
+++ get-glitor.sh
@@ -3,22 +3,23 @@
 echo "Export Environment Variables..."
 export APP_ENVIRONMENT=STG
 # We have to set this env explicitly in row data of ec2
-# export SLACK_WEBHOOK_URL=
+export SLACK_WEBHOOK_URL=''
 export ALLOWED_HOSTS=["*"]
 echo "Environment Variables Exported"
 
 echo "Glitor Downloading..."
-curl --silent "https://api.github.com/repos/gigalogy/glitor/releases/latest" \
+curl -u $USER --silent "https://api.github.com/repos/gigalogy/glitor/releases/latest" \
 | grep '"tag_name":' \
 | sed -E 's/.*"([^"]+)".*/\1/' \
 | xargs -I {} curl -sOL "https://github.com/gigalogy/glitor/archive/"{}'.tar.gz'
 
 echo "Glitor Downloaded!"
 
-tarball="$(find . -name "*.tar.gz")"
-tar -xzf $tarball
+tarball="$(find . -maxdepth 1 -name "*.tar.gz")"
+tar --owner 0 -xzf $tarball
 
 mv glitor-* glitor
+chown -R $USER glitor
 
 cd glitor
 
